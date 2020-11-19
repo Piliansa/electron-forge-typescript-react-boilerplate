97a36bf81d542966cf794d3edfef52d5a5912798
Manu Sabarrós <manusabarros@gmail.com>
2020-11-18 22:29:23 -0300

    fix semantic html tags

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -3,4 +3,4 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./components/App/App";
 
-ReactDOM.render(<App />, document.querySelector("main"));
+ReactDOM.render(<App />, document.getElementById("root"));
